640287f141a6c2a610c40973ee11e535f9b07c09
Jinho Jang <verycosy@kakao.com>
2022-07-21 18:17:18 +0900

    Copy url

--- src/components/result/ShareButton.tsx
+++ src/components/result/ShareButton.tsx
@@ -1,15 +1,18 @@
 import { GA4 } from '../../utils/ga';
 
 export function ShareButton() {
-  const share = () => {
+  const share = async () => {
+    const url = window.location.href;
+
     if (window.navigator.share) {
       window.navigator.share({
-        url: window.location.href,
+        url,
         title: '사랑의 5가지 언어 테스트',
         text: '당신의 사랑의 언어는?',
       });
     } else {
-      // copy url
+      await window.navigator.clipboard.writeText(url);
+      alert('주소가 복사되었습니다.');
     }
 
     GA4.event('share');
